7bd5c4de239d84b58b05d9510fdf039e0a67ff2a
c-b94 <cedricboudreaux@yahoo.com>
2023-05-03 11:28:57 -0500

    end of class

--- app.js
+++ app.js
@@ -3,21 +3,67 @@ const morgan = require("morgan");
 const volleyball = require("volleyball");
 const app = express();
 const postBank = require("./postBank");
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   const posts = postBank.list();
   const html = `<!DOCTYPE html>
   <html>
-    <head>
-    <title>wizard news</title>
-    </head>
-    <body>
-      <ul>
-      ${posts.map(post =>`<li>${post.title}, ${post.name}</li>`)}
-      </ul>
-    </body>
-  </html>`
+  <head>
+    <title>Wizard News</title>
+    <link rel="stylesheet" href="/style.css" />
+  </head>
+  <body>
+    <div class="news-list">
+      <header><img src="/logo.png"/>Wizard News</header>
+      ${posts
+        .map(
+          (post) => `
+        <div class='news-item'>
+          <p>
+            <span class="news-position">${post.id}. ▲</span>
+            <a href="/posts/${post.id}">${post.title}</a>
+            <small>(by ${post.name})</small>
+          </p>
+          <small class="news-info">
+            ${post.upvotes} upvotes | ${post.date}
+          </small>
+          
+        </div>`
+        )
+        .join("")}
+    </div>
+  </body>
+</html>`;
 
+  res.send(html);
+});
+app.get("/posts/:id", (req, res) => {
+  const id = req.params.id;
+  const post = postBank.find(id);
+  const html = `<!DOCTYPE html>
+  <html>
+  <head>
+    <title>Wizard News</title>
+    <link rel="stylesheet" href="/style.css" />
+  </head>
+  <body>
+    <div class="news-list">
+      <header><img src="/logo.png"/>Wizard News</header>
+      
+        <div class='news-item'>
+          <p>
+            <span class="news-position">${post.id}. ▲</span>
+            ${post.title}
+            <small>(by ${post.name})</small>
+          </p>
+          <small class="news-info">
+            ${post.upvotes} upvotes | ${post.date}
+          </small>
+        </div>
+      
+  </body>
+</html>`;
   res.send(html);
 });
 
@@ -26,4 +72,4 @@ const PORT = 1337;
 app.listen(PORT, () => {
   console.log(`App listening in port ${PORT}`);
 });
-app.use(morgan('Dev'));
+app.use(morgan("Dev"));
