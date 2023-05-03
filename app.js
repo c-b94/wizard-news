d45fa07ce6d75d7f9cfb5e6e40c6532513d28f37
c-b94 <cedricboudreaux@yahoo.com>
2023-05-03 10:58:11 -0500

    wizard names and titles

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
 const express = require("express");
+const morgan = require("morgan");
+const volleyball = require("volleyball");
 const app = express();
+const postBank = require("./postBank");
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/", (req, res) => {
+  const posts = postBank.list();
+  const html = `<!DOCTYPE html>
+  <html>
+    <head>
+    <title>wizard news</title>
+    </head>
+    <body>
+      <ul>
+      ${posts.map(post =>`<li>${post.title}, ${post.name}</li>`)}
+      </ul>
+    </body>
+  </html>`
+
+  res.send(html);
+});
 
 const PORT = 1337;
 
 app.listen(PORT, () => {
   console.log(`App listening in port ${PORT}`);
 });
+app.use(morgan('Dev'));
