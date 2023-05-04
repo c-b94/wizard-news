642f38f951de822ab5f33600b9c8a10f305fad41
c-b94 <cedricboudreaux@yahoo.com>
2023-05-04 10:15:06 -0500

    deployment

--- app.js
+++ app.js
@@ -41,7 +41,26 @@ app.get("/", (req, res) => {
 app.get("/posts/:id", (req, res) => {
   const id = req.params.id;
   const post = postBank.find(id);
-  const html = `<!DOCTYPE html>
+  if (!post.id) {
+    // If the post wasn't found, set the HTTP status to 404 and send Not Found HTML
+    res.status(404);
+    const html = `
+     <!DOCTYPE html>
+     <html>
+     <head>
+       <title>Wizard News</title>
+       <link rel="stylesheet" href="/style.css" />
+     </head>
+     <body>
+       <header><img src="/logo.png"/>Wizard News</header>
+       <div class="not-found">
+         <p>404: Page Not Found</p>
+       </div>
+     </body>
+     </html>`;
+    res.send(html);
+  } else {
+    const html = `<!DOCTYPE html>
   <html>
   <head>
     <title>Wizard News</title>
@@ -64,7 +83,12 @@ app.get("/posts/:id", (req, res) => {
       
   </body>
 </html>`;
-  res.send(html);
+    res.send(html);
+  }
+});
+
+app.get("*", (req, res, next) => {
+  res.status(404).send("Oops, that endpoint doesn't exist!");
 });
 
 const PORT = 1337;
